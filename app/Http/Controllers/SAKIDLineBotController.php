b677f5374b1d958b9119209192ea2b2e882f7be0
koramit <koramit.p@gmail.com>
2018-06-15 10:31:37 +0700

    refactoring LINE controller

--- app/Http/Controllers/SAKIDLineBotController.php
+++ app/Http/Controllers/SAKIDLineBotController.php
@@ -7,7 +7,7 @@ use App\SAKIDLineBot;
 // use App\ServiceDomain;
 use App\LINEBotManager;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 
 class SAKIDLineBotController extends Controller
 {
@@ -36,12 +36,10 @@ class SAKIDLineBotController extends Controller
     {
         // return ['code' => 0];
         if ($request->has('events')) {
-            Log::info(json_encode($request->input('events')));
+            Cache::put('line-webhook', $request->input('events'), 5);
             $this->events = $request->input('events');
             $botManager = new LINEBotManager($request->input('events'));
             return $botManager->handleEvents(SAKIDLineBot::find($botId));
-        } else {
-            Log::info('no events');
         }
         // return (new LINEBotManager($request))->handleEvents(SAKIDLineBot::find($botId));
     }
